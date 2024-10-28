739f36c5d30ff2af7b53469adfe52432fb6050be
JKentTech <contact@jkent.tech>
2024-10-29 00:27:41 +1100

    refactored controller logging

--- routes/rootRoute.js
+++ routes/rootRoute.js
@@ -1,14 +1,15 @@
 import express from 'express';
 import * as _config from '../utils/config.js';
 import * as _controller from '../controllers/rootController.js';
+import * as _apiController from '../controllers/rootApiController.js';
 
 export const router = express.Router();
 
 router.get(_config.rootEndpoint, (req, res)=>{
-    _controller.root(req, res, "[GET]", _config.rootEndpoint);
+    _controller.root(req, res);
 });
 
 router.get(_config.api(_config.rootEndpoint), (req, res)=>{
-    res.send(_config.api(_config.rootEndpoint));
+    _apiController.root(req, res);
 });
 
