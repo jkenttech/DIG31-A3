4c56d531b74743a2a9f67676cb38a05ca6e5278d
JKentTech <contact@jkent.tech>
2024-10-30 01:47:13 +1100

    added login form with placeholder error messages

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -2,5 +2,8 @@ import { _log } from '../app.js';
 
 export function root(req, res){
     _log.write_request(req);
-    res.render('login');
+    let messages = { };
+    messages.login = "No login provided";
+    messages.password = "No password provided";
+    res.render('login', { errors: messages });
 }
