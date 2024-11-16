a3838276943e61348f5918ad5d58b1cc36b2d4f6
JKentTech <contact@jkent.tech>
2024-11-16 13:07:40 +1100

    restarting project

--- controllers/registerApiController.js
+++
@@ -1,6 +0,0 @@
-import { _log } from '../app.js';
-
-export function root(req, res){
-    _log.write_request(req);
-    res.send(req.path);
-}
--- utils/database.js
+++
@@ -1,2 +0,0 @@
-import * as _config from './utils/config.js';
-
