e2acc1d5da234ac0ee2bab551b03724312606567
JKentTech <contact@jkent.tech>
2024-11-19 22:42:46 +1100

    added checks to backend user route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,13 +1,39 @@
 import express from 'express';
 import * as _config from '../utils/config.js';
 import { _log } from '../app.js';
+import { User } from '../models/User.js';
 
 export const router = express.Router();
 
 router.post(_config.rootEndpoint, (req, res)=>{
     _log.write(_log.INF, `JSON ${JSON.stringify(req.body)}`);
-    _log.write(_log.INF, `Body email ${req.body.email}`);
-    res.status(200);
-    res.send();
-});
 
+    if(Object.keys(req.body).length === 0){   
+        return res.status(400).send({message: "User content can not be empty"})
+    }
+
+    // check account with email doen't already exist
+    User.findOne({email: req.body.email})
+    .then(user => {
+        if( user != null ){
+        return res.status(400).json({
+            message: "email already in use, use different email address"
+        })
+        }
+    // create new user       
+    let newUser = new User(req.body)
+    newUser.save()
+        .then(user => {        
+        // success!  
+        // return 201 status with user object
+        return res.status(201).json(user)
+        })
+        .catch(err => {
+        console.log(err)
+        return res.status(500).send({
+            message: "Problem creating account",
+            error: err
+        })
+        })
+    })
+});
