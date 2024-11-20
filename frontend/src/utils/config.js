4e5fb7068e4726d45843f08b66c7b827097720b6
JKentTech <contact@jkent.tech>
2024-11-21 01:46:14 +1100

    remove api base from config

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,8 +1,4 @@
 // Config
-const protocol = "http";
-const ip = "ec2-3-107-19-145.ap-southeast-2.compute.amazonaws.com";
-const port = 80;
-export const apiBase = `${protocol}//${ip}:${port}`;
 export const appName = "Simple Logbook";
 export const version = "1.0.0";
 
